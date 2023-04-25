56802e3273fecc93ce7d62a6fdbcc7e65c0f5ae3
fymorGod <mufinagamer@gmail.com>
2023-04-25 17:01:32 -0300

    feat: insert URL dinamic :zap:

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, session, flash
 class Jogo:
     def __init__(self, name:str, category:str, console:str):
         self.name = name
@@ -7,6 +6,7 @@ class Jogo:
         self.console = console
 
 app = Flask(__name__)
+app.secret_key = 'genshin' 
 
 game_one = Jogo('Genshin', 'Adventure', 'PC')
 game_two = Jogo('Elden Ring', 'RPG/Adventure', 'Xbox/PS5')
@@ -20,6 +20,8 @@ def index():
 
 @app.route('/novo')
 def novo():
+    if 'user' not in session or session['user'] == None:
+        return redirect('/login?proxima=novo')
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -31,6 +33,28 @@ def criar():
     #create object 
     game = Jogo(name=nome, category=categoria, console=console)
     lista.append(game)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    proxima = request.args.get('proxima')
+    return render_template('login.html', proxima=proxima)
+
+@app.route('/autenticar', methods=['POST',])
+def auth():
+    if 'alohomora' == request.form['senha']:
+      session['user'] = request.form['usuario']
+      flash(session['user'] + ' authentication sucesses!')
+      next_page = request.form['proxima']
+      return redirect('/{}'.format(next_page))
+    else:
+      flash('User is not authenticated')        
+      return redirect('/login')    
+    
+@app.route('/logout')
+def logout():
+    session['user'] = None
+    flash('User is left!')
     return redirect('/')
 
 app.run(debug=True, host='0.0.0.0', port=8080)
