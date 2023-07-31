79c4b3433458ff5b7f0ade5cceacd773b9a97faf
fymorGod <mufinagamer@gmail.com>
2023-07-30 22:32:41 -0300

    feat: complete insert db configuration and the creation tables on database

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 from flask import Flask, render_template, request, redirect, session, flash, url_for
-class Jogo:
-    def __init__(self, name:str, category:str, console:str):
-        self.name = name
-        self.category = category
-        self.console = console
-
-class User:
-    def __init__(self, name, nickname, password):
-        self.name = name
-        self.nickname = nickname
-        self.password = password
-
-user1 = User('fylip', 'mufina', 'honkai')
-user2 = User('camila', 'nila', 'paozinho')
-user3 = User('tomas', 'tom', 'python_is_life')
-
-users = {
-    user1.nickname: user1,
-    user2.nickname: user2,
-    user3.nickname: user3
-}
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'genshin' 
 
-game_one = Jogo('Genshin', 'Adventure', 'PC')
-game_two = Jogo('Elden Ring', 'RPG/Adventure', 'Xbox/PS5')
-game_three = Jogo('Mario', 'Arcade', 'Nintendo')
+#configuração de conexão com o db
+app.config["SQLALCHEMY_DATABASE_URI"] = \
+    "{SGBD}://{user}:{password}@{server}/{database}".format(
+        SGBD = "mysql+mysqlconnector",
+        user = "root",
+        password = "root",
+        server = "localhost",
+        database = "jogoteca"
+    )
+#instancia do db com SQLAlchemy
+db = SQLAlchemy(app)
 
-lista = [game_one, game_two, game_three]
+class Jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)
+    category = db.Column(db.String(40), nullable=False)
+    console = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return "<Name %r>" % self.name
+    
+class Usuarios(db.Model):
+    nome = db.Column(db.String(20), nullable=False)
+    nickname = db.Column(db.String(8), nullable=False, primary_key=True)
+    senha = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return "<Name %r>" % self.name
 
 @app.route('/')
 def index():
+    lista = Jogos.query.order_by(Jogos.id)
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 @app.route('/novo')
@@ -45,10 +49,17 @@ def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
+
+    jogo = Jogos.query.filter_by(nome=nome).first()
+    if jogo:
+        flash("Jogo is already exists")
+        return redirect(url_for('index'))
     
-    #create object 
-    game = Jogo(name=nome, category=categoria, console=console)
-    lista.append(game)
+    #instanciar a criação de um novo jogo
+    novo_jogo = Jogos(name=nome, category=categoria, console=console)
+    db.session.add(novo_jogo)
+    db.session.commit()
+
     return redirect(url_for('index'))
 
 @app.route('/login')
@@ -58,9 +69,9 @@ def login():
 
 @app.route('/autenticar', methods=['POST',])
 def auth():
-    if request.form['usuario'] in users:
-        usuario = users[request.form['usuario']]
-        if request.form['senha'] == usuario.password:
+    usuario = Usuarios.query.filter_by(nickname=request.form['usuario']).first()
+    if usuario:
+        if request.form['senha'] == usuario.senha:
             session['user'] = usuario.nickname
             flash(usuario.nickname + ' authentication sucesses!')
             next_page = request.form['proxima']
